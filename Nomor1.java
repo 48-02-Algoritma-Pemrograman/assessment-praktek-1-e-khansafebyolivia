b3bdb789e1b48b5b0a5a2ea74df91edf590f052d
khansa feby olivia <khansafebyy@gmail.com>
2024-10-24 14:29:45 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,18 @@
 public class Nomor1{
     public static void main(String[] args){
+Scanner scanner = new Scanner (System.in);
 
+int jumlahAnggota;
+double tunjangan;
+
+System.out.print("Masukkan jumlah anggota keluarga: ");
+         jumlahAnggota= scanner.nextInt();
+
+if (jumlahAnggota <= 3) {
+            tunjangan = jumlahAnggota * 100.000;
+        } else {
+            tunjangan = (3 * 100.000) + ((jumlahAnggota - 3) * 50.000);
+        }
+System.out.println("Besar tunjangan kesehatan :" + (tunjangan));
     }
 }
